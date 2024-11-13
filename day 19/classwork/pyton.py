6cbfb8a1468b1d3d44522f663bf6e73d89c1f116
sabagigani <giganisaba54@gmail.com>
2024-11-13 22:14:08 +0400

    meoce commit

---
+++ day 19/classwork/pyton.py
@@ -0,0 +1,7 @@
+name= ["saba","gurami","gio"]
+print(name[-1:4])
+name=["name","why","nah","shemeh sheme a shemea shemea draik","nah i'd win"]
+print(name[1:3])
+
+name=["ლამაზი","კარგად გაკეთებული","გაფუტებული,დანგრეული","კორფუსი","პატარა"]
+print(name[2:6])
---
+++ day 19/homework/puton.py
@@ -0,0 +1,12 @@
+name=["saba","gio","vano","Cakvetaze","mamardsvili","kvaracxelia","fati","xvica","andria","nia"]
+print(name[-10:11])
+
+
+for i in range(5):
+    print("saba")
+
+num=input("what is your age")
+
+for i in range (5):
+    print(num)
+
