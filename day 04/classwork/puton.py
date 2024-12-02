af9a1144f47849d0b8c8f21d934efe7c7bdbbf52
sabagigani <giganisaba54@gmail.com>
2024-12-02 19:07:54 +0400

    meocdametxotmede commit

---
+++ day 04/classwork/puton.py
@@ -0,0 +1,4 @@
+input("what is your name")
+input("what is your surname")
+input("what is your age")
+print("input" + 5)
---
+++ day 24/classwork/pyton.py
@@ -0,0 +1,5 @@
+for i in range(10):
+    print(i)
+
+name=["gio","nika","saba","andria","sandro"]
+print(name[-2:3])
