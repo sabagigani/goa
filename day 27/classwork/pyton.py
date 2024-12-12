71cc82c8f0c47f96016ad7e41e29f259746d9ebc
sabagigani <giganisaba54@gmail.com>
2024-12-12 20:27:14 +0400

    mepcdameevre commit

---
+++ day 27/classwork/pyton.py
@@ -0,0 +1,9 @@
+
+def add(num1 ,num2):
+     print(num1 *num2)
+     add(55,9)
+
+
+def substract(num1,num2):
+     print(num1-num2)
+substract(500,100)
---
+++ day 27/homework/poto.py
@@ -0,0 +1,30 @@
+def motiply(x,z):
+     x = x*z
+     return x
+     print(x*z)
+print(motiply(123,231))  
+
+
+
+
+
+
+def name(x):
+    print(f"what is your name?")
+    print(f"my name is {x}")
+    print(f"hi {x}")
+name("giorgi")
+
+
+
+
+
+
+def creati_name(first,last):
+     first = first.capitalize()
+     last = last.capitalize()
+     return first + " " + last
+full_name = creati_name("saba","gigani")
+print(full_name)
+
+     
