5c8c109f8bfd3ba390e0117affb2db5d76bfe3eb
sabagigani <giganisaba54@gmail.com>
2025-01-28 18:10:43 +0400

    ormocdameora commit

---
+++ day 40/classwork/pyton.py
@@ -0,0 +1,13 @@
+myTuple = ("John", "Peter", "Vicky")
+
+x = "#".join(myTuple)
+
+print("mytuple")
+
+email= input("type youre email heare =")
+password=input("type your email heare =")
+
+for i in range(email):
+  password.append(int(input()))
+
+print(&quot;List of numbers:&quot;, number_list)
--- nah id win.py
+++ nah id win.py
@@ -1,1 +1,40 @@
-print(len(name))
+def brainfuck_to_python(bf_code):
+    python_code = [
+        "memory = [0] * 30000",
+        "ptr = 0",
+        "def debug_memory():",
+        "    print(f'ptr: {ptr}, memory: {memory[:10]}')"
+    ]
+    bf_to_py = {
+        '>': 'ptr += 1',
+        '<': 'ptr -= 1',
+        '+': 'memory[ptr] += 1',
+        '-': 'memory[ptr] -= 1',
+        '.': 'print(chr(memory[ptr]), end="")',
+        ',': 'memory[ptr] = ord(input()[0])',
+        '[': 'while memory[ptr] != 0:\n    debug_memory()',
+        ']': '    debug_memory()'  # Added debug_memory() to trace the exit condition
+    }
+    indent_level = 0
+    for command in bf_code:
+        if command in bf_to_py:
+            if command == ']':
+                indent_level -= 1
+            code_line = '    ' * indent_level + bf_to_py[command]
+            python_code.append(code_line)
+            if command == '[':
+                indent_level += 1
+    return '\n'.join(python_code)
+
+
+bf_code = "++[>++<-]>."
+
+
+transpiled_code = brainfuck_to_python(bf_code)
+print("Transpiled Python Code:\n", transpiled_code)
+
+
+try:
+    exec(transpiled_code)
+except Exception as e:
+    print("Error during execution:", e) 
