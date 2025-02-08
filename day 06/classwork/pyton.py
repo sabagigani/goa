ad8df7987d08adee77a0a506a1d11874c7be51be
sabagigani <giganisaba54@gmail.com>
2025-02-08 21:56:49 +0400

    meorcdamoere commit

---
+++ day 06/classwork/pyton.py
@@ -0,0 +1,13 @@
+
+text = "გამარჯობა, მსოფლიო!"
+
+
+number = 42
+
+
+decimal = 3.14
+
+
+print("String ტიპის ცვლადი:", text)
+print("Integer ტიპის ცვლადი:", number)
+print("Float ტიპის ცვლადი:", decimal)
